e28ddea290a49a55efdc8db768f0cf9817f8986e
Abdul-khalid-2 <121563551+Abdul-khalid-2@users.noreply.github.com>
2026-08-19 19:14:21 +0500

    Add JSON post import and export

--- routes/web.php
+++ routes/web.php
@@ -36,6 +36,9 @@ Route::middleware(['auth', 'workspace.context'])->group(function (): void {
     Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('create', [CreatePostController::class, 'index'])->name('create');
     Route::get('posts', [PostsController::class, 'index'])->name('posts.index');
+    Route::get('posts/export', [PostsController::class, 'export'])->name('posts.export');
+    Route::get('posts/import-template', [PostsController::class, 'importTemplate'])->name('posts.import-template');
+    Route::post('posts/import', [PostsController::class, 'import'])->name('posts.import');
     Route::get('social-posts', [SocialPostsController::class, 'index'])->name('social-posts.index');
     Route::post('social-posts/refresh-stats', [SocialPostsController::class, 'refreshStats'])->name('social-posts.refresh-stats');
     Route::get('social-posts/targets/{target}/comments', [SocialPostsController::class, 'comments'])
